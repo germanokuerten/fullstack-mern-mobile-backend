f4310e2fd50cb24e6d54dddc3944f178ea0be127
Germano Kuerten <germanokuerten@gmail.com>
2022-06-06 19:31:15 -0700

    database connection

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 // Setup Dependencies
 require("dotenv").config()
 
+const {PORT = 3334, DATABASE_URL }=process.env
 const express = require("express")
 const mongoose = require("mongoose")
 const morgan = require("morgan")
@@ -17,4 +18,22 @@ app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json)
 
+// Database connection
+mongoose.connect(process.env.DATABASE_URL)
 
+mongoose.connection
+    .on("open", () => console.log("MongoDB Connected"))
+    .on("closed", () => console.log("MongoDB Closed"))
+    .on("error", (error) => console.log(error))
+
+// Schema Model
+const RestaurantSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    link: String,
+    menu1: String,
+    menu2: String,
+    menu3: String
+})
+
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema)
